1a69bccbf8c50799ed1d5b78e0a4fa959b9f2d24
jonasdasneves <jonas.joliveira2024@gmail.com>
2025-06-02 20:57:42 -0300

    Mudança de URL no config para geração de token

--- src/main/java/br/com/fiap/service/UsuarioService.java
+++ src/main/java/br/com/fiap/service/UsuarioService.java
@@ -33,7 +33,7 @@ public class UsuarioService {
         // Duração do token em segundos (ex: 1 hora)
         long duration = 3600;
 
-        return Jwt.issuer("https://stuck-grace-jonasdasneves.koyeb.app/")
+        return Jwt.issuer("http://localhost:8080")
                 .upn(usuario.nome)
                 .groups(usuario.cargo)
                 .expiresAt(System.currentTimeMillis() / 1000 + duration)
