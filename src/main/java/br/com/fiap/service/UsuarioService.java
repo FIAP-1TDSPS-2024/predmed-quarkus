cd055eed3bf18fd7c985263ac280cf708bf9f76f
jonasdasneves <jonas.joliveira2024@gmail.com>
2025-06-02 22:00:35 -0300

    Mudança de URL no config para geração de token

--- src/main/java/br/com/fiap/service/UsuarioService.java
+++ src/main/java/br/com/fiap/service/UsuarioService.java
@@ -33,7 +33,7 @@ public class UsuarioService {
         // Duração do token em segundos (ex: 1 hora)
         long duration = 3600;
 
-        return Jwt.issuer("https://predmed-quarkus.koyeb.app")
+        return Jwt.issuer("https://predmed-quarkus-01.koyeb.app")
                 .upn(usuario.nome)
                 .groups(usuario.cargo)
                 .expiresAt(System.currentTimeMillis() / 1000 + duration)
